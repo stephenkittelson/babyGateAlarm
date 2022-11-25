ec67ccd38324ffb7a24ff0db3d2afee32a538c62
Stephen Kittelson <stephen.kittelson@gmail.com>
2022-11-25 11:58:19 -0700

    Instead of permanently disabling alarm until next button push, just disable alarm for 30 seconds and blink the light until it's re-armed. Also shorten sound so it reacts more quickly to button pushes.

--- BabyGateAlarm.ino
+++ BabyGateAlarm.ino
@@ -1,42 +1,69 @@
 const int SPEAKER = 8;
 const int GATE_SENSOR = 2;
-const int ARM_TOGGLE = 4;
+const int DISARM = 4;
 const int ARMED_LIGHT = 7;
 
-bool isArmed = true;
+const int STATE_HIGH = 5;
+const int STATE_LOW = 6;
+const int STATE_UNCHANGED = 7;
 
-int lastArmToggleReading = LOW;
-int timeOfLastArmToggleChange = 0;
-bool readButtonPress = false;
-int currentArmToggleState = LOW;
-const int DEBOUNCE_DELAY = 50;
+bool isArmed = true;
+unsigned int timeOfLastDisarmPush = 0;
+
+unsigned int timeOfLastLightChange = 0;
+int currentLightState = LOW;
 
 void setup() {
   pinMode(SPEAKER, OUTPUT);
   pinMode(GATE_SENSOR, INPUT);
-  pinMode(ARM_TOGGLE, INPUT);
+  pinMode(DISARM, INPUT);
   pinMode(ARMED_LIGHT, OUTPUT);
 }
 
 void loop() {
-  int currentArmToggleReading = digitalRead(ARM_TOGGLE);
-  if (currentArmToggleReading != lastArmToggleReading) {
-    timeOfLastArmToggleChange = millis();
-  } else if ((millis() - timeOfLastArmToggleChange) > DEBOUNCE_DELAY && currentArmToggleReading != currentArmToggleState) {
-    currentArmToggleState = currentArmToggleReading;
-    if (currentArmToggleState == HIGH) {
-      isArmed = !isArmed;
-    }
+  unsigned int currentTime = millis();
+  if (getDebouncedDisarmSwitchState() == STATE_HIGH) {
+    timeOfLastDisarmPush = currentTime;
+    isArmed = false;
   }
-  lastArmToggleReading = currentArmToggleReading;
+  if (!isArmed && (currentTime - timeOfLastDisarmPush) > 30000) {
+      isArmed = true;
+  }
 
-  if (isArmed) {
-    digitalWrite(ARMED_LIGHT, HIGH);
-  } else {
-    digitalWrite(ARMED_LIGHT, LOW);
+  if (isArmed && currentLightState == LOW) {
+    currentLightState = HIGH;
+    digitalWrite(ARMED_LIGHT, currentLightState);
+  } else if (!isArmed) {
+    if ((currentTime - timeOfLastLightChange) > 1000) {
+      currentLightState = !currentLightState;
+      digitalWrite(ARMED_LIGHT, currentLightState);
+      timeOfLastLightChange = currentTime;
+    }
   }
 
   if (isArmed && digitalRead(GATE_SENSOR) == LOW) {
-    tone(SPEAKER, 100, 1000);
+    tone(SPEAKER, 100, 100);
   }
+}
+
+int lastDisarmReading = LOW;
+int timeOfLastDisarmChange = 0;
+int currentDisarmState = LOW;
+const int DEBOUNCE_DELAY = 50;
+
+int getDebouncedDisarmSwitchState() {
+  int currentDisarmReading = digitalRead(DISARM);
+  if (currentDisarmReading != lastDisarmReading) {
+    timeOfLastDisarmChange = millis();
+  } else if ((millis() - timeOfLastDisarmChange) > DEBOUNCE_DELAY && currentDisarmReading != currentDisarmState) {
+    currentDisarmState = currentDisarmReading;
+    switch (currentDisarmState) {
+      case HIGH:
+        return STATE_HIGH;
+      case LOW:
+        return STATE_LOW;
+    }
+  }
+  lastDisarmReading = currentDisarmReading;
+  return STATE_UNCHANGED;
 }
